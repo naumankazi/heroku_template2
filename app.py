e85c3fba9baa44406878d03c4754a5cdeef003b9
Kazi Nauman <69042868+naumankazi@users.noreply.github.com>
2021-04-08 22:53:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
-import sys
-
 import numpy as np
-from flask import Flask, request, render_template, logging
+from flask import Flask, request, render_template
 import pickle
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
-
-app.logger.setLevel(logging.ERROR)
 
 
 @app.route('/')
